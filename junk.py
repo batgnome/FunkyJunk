84a0756ec353ef8db4938d226e2769a9c8a2d141
g0730n <114636821+g0730n@users.noreply.github.com>
2023-08-07 15:05:40 -0700

    Update junk.py

--- junk.py
+++ junk.py
@@ -1,5 +1,5 @@
-from time import sleep
+from time import sleep #import sleep function from time module
 
-while True:
-  print('funky')
-  sleep(1)
+while True:         #endless loop
+    print('funky')  #print to console
+    sleep(1)        #sleep 1 second
