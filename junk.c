806279a5b369ac54cef051fb166d6025ed100a97
g0730n <114636821+g0730n@users.noreply.github.com>
2023-08-07 15:01:49 -0700

    Update and rename funky.c to junk.c

---
+++ junk.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/*lets get some examples and use of
+some:
+[] variables
+[] functions
+[] temporary data storage (eg. array)
+[] persistant data storage (write/read a file)
+[] document/comment what is doing what
+*/
+int main() //main function
+{
+    for(;;) //endless for loop
+    {
+        printf("Funky!\n"); //print to console
+        sleep(1); //sleep 1 second
+    }
+    return 0; //program exit code
+}
